3b7b40227a04f3899c63781d13d636ee262fe02c
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-11-24 02:03:16 -0500

    add the route of achievement and reset password form to the application

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUpForm from "./components/signup";
 import SignInPage from "./components/login";
+import ResetPasswordForm from "./components/resetpass";
 import Workout from "./components/workout";
 import NutritionDashboard from "./components/nutrition";
 import SleepTracker from "./components/sleep";
 import HomePage from "./components/home";
+import AchievementComponent from "./components/achievement";
 
 const App = () => {
   return (
@@ -14,10 +16,11 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<SignInPage />} />
+        <Route path="/resetpass" element={<ResetPasswordForm />} />
         <Route path="/sleep" element={<SleepTracker />} />
         <Route path="/nutrition" element={<NutritionDashboard />} />
         <Route path="/workout" element={<Workout />} />
-        <Route path="/achievement" element={<h1>Achievement</h1>} />
+        <Route path="/achievement" element={<AchievementComponent />} />
       </Routes>
     </Router>
   );
